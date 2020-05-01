928cd8712c1b29005039b75c159f281b83bfe712
james kee <43153742+jamesthekee@users.noreply.github.com>
2020-05-01 16:16:48 +0100

    Add files via upload

--- quintballs/scripts/sketch.js
+++ quintballs/scripts/sketch.js
@@ -8,22 +8,28 @@ var drag = 0.925;
 var conversion_range=16
 var gravity_range=40000
 
-var count = 50;
+var count = 100;
+var start_distance = 10;
 
 
 function setup() {
     createCanvas(1200, 800);
     noStroke();
 
-    particles = [];
-    for (var i = 0; i < count; i++) {
-        particles[i] = new Particle(random(width)/2 + width/4, random(height)/2 + height/4, 3, int(random(5)), 255);
+    for(var t=0; t<types; t++){
+        var angle = t*TWO_PI/types;
+        var dis = start_distance;
+
+        for (var i = 0; i < count; i++) {
+            particles.push(new Particle(width/2 + dis*cos(angle), height/2 + dis*sin(angle), 3,  t, 255));
+            dis += 2;
+            angle += 0.125;
+        }
     }
 }
 
 function draw() {
     background(0);
-
 
     for (var i = 0; i < particles.length; i++) {
         particles[i].applyGravity(particles);
@@ -32,10 +38,9 @@ function draw() {
         particles[i].bounce();
         particles[i].draw();
     }
+    //console.log(frameRate())
 }
 
 function mousePressed(){
-    //particles.push(new Particle(mouseX, mouseY, 4, 3, int(random(5)), 255));
-    particles.push(new Particle(mouseX, mouseY, 3, int(random(5)), 255));
-    console.log(particles);
+    particles.push(new Particle(mouseX, mouseY, 5, int(random(5)), 255));
 }
