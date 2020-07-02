c70c447be6f209b20c0fc3774085cda7ff7e9118
james kee <43153742+jamesthekee@users.noreply.github.com>
2020-07-02 14:28:08 +0100

    Removed clutter

--- amazons/scripts/amazonsgame.js
+++ amazons/scripts/amazonsgame.js
@@ -7,15 +7,10 @@ var debug = false;
 
 function setup() {
     var canvas = createCanvas(1200, 800);
-    //canvas.parent('sketch-div');
     textSize(32);
     
     current_board = new board(board_size, board_width, debug);
     current_board.draw();
-}
-
-function draw(){
-
 }
 
 function mousePressed(){
